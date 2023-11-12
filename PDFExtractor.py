2c7c13bd82d9b66f50aab8ab792e945d5dba1a49
EmanueleCeglia <emanueleceglia0@gmail.com>
2023-11-12 19:44:37 +0100

    add try and expect and sleep time for openai errors

--- PDFExtractor.py
+++ PDFExtractor.py
@@ -5,13 +5,17 @@ from pdf_extractor import PDFDeductiblesFinder
 from postprocessing_functions import postprocess_and_clean_dates
 from tqdm import tqdm
 import json
-
+from datetime import datetime
+import time
 
 # OpenAI model settings
 API_KEY = Path("credentials/openai_api_key.txt").read_text().strip()
 openai.api_key = API_KEY
 model_id4 = 'gpt-4-1106-preview'
 
+
+# time start
+start = datetime.now()
 
 
 # ChatGPT-4
@@ -91,63 +95,78 @@ output_dictionary = {}
 # Iterate over all files in the insurances folder
 for file_path in insurances_folder_path.iterdir():
     
-    if file_path.is_file():
-        
-        # Identification dates
-        extractor_dates = PDFDatesFinder(file_path, API_KEY)
-        pages = extractor_dates.extract_text()
-        text_pages_dates, index_pages_dates = extractor_dates.identify_period_pages(pages)
+    try:
+        # file name and current time
+        print(f"File name: {file_path.stem} | Time: {datetime.now()}")
 
-        # Dates extraction using GPT
-        output_dates = []
+        if file_path.is_file():
+            
+            # Identification dates
+            extractor_dates = PDFDatesFinder(file_path, API_KEY)
+            pages = extractor_dates.extract_text()
+            text_pages_dates, index_pages_dates = extractor_dates.identify_period_pages(pages)
 
-        for page in text_pages_dates:
-                
-                conversation = []
+            # Dates extraction using GPT
+            output_dates = []
 
-                prompt = compose_prompt_date(
-                    text=page,
-                )
-                conversation.append({'role': 'user', 'content': prompt})
-                conversation = ChatGPT4_conversation(conversation)
+            for page in text_pages_dates:
+                    
+                    conversation = []
 
-                response = conversation[-1]['content'].strip()
+                    prompt = compose_prompt_date(
+                        text=page,
+                    )
+                    conversation.append({'role': 'user', 'content': prompt})
+                    conversation = ChatGPT4_conversation(conversation)
 
-                if response:
-                    output_dates.append(response)
+                    response = conversation[-1]['content'].strip()
 
-        # Postprocessing dates
-        output_dates = postprocess_and_clean_dates(output_dates)
-        
-        
-        # Identification deductibles
-        extractor_deductibles = PDFDeductiblesFinder(file_path, API_KEY)
-        pages = extractor_deductibles.extract_text()
-        text_pages_deductibles, index_pages_deductibles = extractor_deductibles.identify_deductibles_pages(pages)
+                    if response:
+                        output_dates.append(response)
 
-        # Deductibles extraction using GPT
-        output_deductibles = []
+            # Postprocessing dates
+            output_dates = postprocess_and_clean_dates(output_dates)
+            
+            
+            # Identification deductibles
+            extractor_deductibles = PDFDeductiblesFinder(file_path, API_KEY)
+            pages = extractor_deductibles.extract_text()
+            text_pages_deductibles, index_pages_deductibles = extractor_deductibles.identify_deductibles_pages(pages)
 
-        for page in text_pages_deductibles:
-                
-                conversation = []
+            # Deductibles extraction using GPT
+            output_deductibles = []
 
-                prompt = compose_prompt_deductibles(
-                    text=page,
-                )
-                conversation.append({'role': 'user', 'content': prompt})
-                conversation = ChatGPT4_conversation(conversation)
+            for page in text_pages_deductibles:
+                    
+                    conversation = []
 
-                response = conversation[-1]['content'].strip()
+                    prompt = compose_prompt_deductibles(
+                        text=page,
+                    )
+                    conversation.append({'role': 'user', 'content': prompt})
+                    conversation = ChatGPT4_conversation(conversation)
 
-                if response:
-                    output_deductibles.append(response)
+                    response = conversation[-1]['content'].strip()
 
-    output_dictionary[file_path.stem] = {'insurance period': output_dates, 'deductibles': output_deductibles}
+                    if response:
+                        output_deductibles.append(response)
+
+        output_dictionary[file_path.stem] = {'insurance period': output_dates, 'deductibles': output_deductibles}
+    
+    except Exception as e:
+        print(f"Error with file: {file_path.stem}")
+        print(e)
+
+    time.sleep(120)
+
+# time end
+end = datetime.now()
 
 # Save output dictionary as json file
 with open('output.json', 'w') as fp:
     json.dump(output_dictionary, fp)
+
+    print(f"Time taken: {end-start}")
 
         
 
